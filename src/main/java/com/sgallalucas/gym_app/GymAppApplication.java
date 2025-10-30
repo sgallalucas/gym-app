b8e8fceef4a0f1dee423a6c9d54a3eceaa2492f9
Lucas Sgalla <lucasgalla10@gmail.com>
2025-10-30 19:50:51 -0300

    Project created

---
+++ src/main/java/com/sgallalucas/gym_app/GymAppApplication.java
@@ -0,0 +1,13 @@
+package com.sgallalucas.gym_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppApplication.class, args);
+	}
+
+}
