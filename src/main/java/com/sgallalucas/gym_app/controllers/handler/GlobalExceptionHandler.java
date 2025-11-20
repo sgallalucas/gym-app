894946682b48582fce10b62d312844529c67c0ce
Lucas Sgalla <lucasgalla10@gmail.com>
2025-11-19 21:50:12 -0300

    Adds new exception handling to the GlobalExceptionHandler

--- src/main/java/com/sgallalucas/gym_app/controllers/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sgallalucas/gym_app/controllers/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sgallalucas.gym_app.controllers.handler;
 import com.sgallalucas.gym_app.controllers.dtos.errors.ErrorResponseDetails;
 import com.sgallalucas.gym_app.controllers.dtos.errors.FieldErrorDetails;
 import com.sgallalucas.gym_app.exceptions.DuplicateRecordException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -31,5 +33,23 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponseDetails handleDuplicateRecordException(DuplicateRecordException ex) {
         return new ErrorResponseDetails(HttpStatus.CONFLICT.value(), ex.getMessage(), LocalDateTime.now(), List.of());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponseDetails handleDateTimeParseException(DateTimeParseException ex) {
+        return new ErrorResponseDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), LocalDateTime.now(), List.of());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDetails handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ErrorResponseDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), LocalDateTime.now(), List.of());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDetails handleUnexpectedError(RuntimeException ex) {
+        return new ErrorResponseDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error", LocalDateTime.now(), List.of());
     }
 }
