35ae782949df67e94df7b1240d37aea536b2bc52
Lucas Sgalla <lucasgalla10@gmail.com>
2025-11-27 19:22:22 -0300

    remove WorkoutDTO for refactoring

--- src/main/java/com/sgallalucas/gym_app/services/WorkoutService.java
+++ src/main/java/com/sgallalucas/gym_app/services/WorkoutService.java
@@ -2,8 +2,11 @@ package com.sgallalucas.gym_app.services;
 
 import com.sgallalucas.gym_app.model.Workout;
 import com.sgallalucas.gym_app.repositories.WorkoutRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +15,9 @@ public class WorkoutService {
 
     public void create(Workout workout) {
         workoutRepository.save(workout);
+    }
+
+    public Workout findById(UUID id) {
+        return workoutRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Workout not found"));
     }
 }
